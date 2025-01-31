787f0237887ef980f1c34b5610433beb65c9557f
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-01-30 23:05:44 -0500

    feat: added function call on submit

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,9 +1,14 @@
 
+const onLoginSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(e)
+}
+
 function LoginForm() {
     return (
         <>
             <div className="container d-flex justify-content-center align-items-center">
-                <form className="p-4 border rounded shadow bg-white">
+                <form className="p-4 border rounded shadow bg-white" onSubmit={onLoginSubmit}>
                     <h2 className="text-center">Sign In</h2>
                     <div className="form-floating mb-3">
                         <input type="text" className="form-control" id="username" placeholder="Enter username" />
