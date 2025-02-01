350dc237cb0ff24b9d4f6c03969ba7da6cbc0190
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-01-31 21:21:08 -0500

    feat: made home page reactive tm

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import LoginForm from "../components/LoginForm";
+import SignUpForm from "../components/SignUpForm";
 
 function Home() {
+    const [isLogin, setIsLogin] = useState(true);
+    let btnMessage = isLogin ? "Don't have an account?" : "Have an account?";
+
+    const getForm = (isLoggingIn: boolean) => isLoggingIn ? <LoginForm /> : <SignUpForm />
+
     return (
         <>
-            <LoginForm />
+            <div className="container d-flex flex-column align-items-center justify-content-center min-vh-100">
+                {getForm(isLogin)}
+                <button 
+                    onClick={(_) => setIsLogin(!isLogin)} 
+                    className="btn btn-primary my-3">
+                    {btnMessage}
+                </button>
+            </div>
         </>
     );
 }
