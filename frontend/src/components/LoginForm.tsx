97f3662b384c87ae6c724d6efceace8d5c5a60e1
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-01-31 21:20:35 -0500

    feat: container logic taken out of sign in form

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -7,22 +7,20 @@ const onLoginSubmit = (e: React.FormEvent) => {
 function LoginForm() {
     return (
         <>
-            <div className="container d-flex justify-content-center align-items-center">
-                <form className="p-4 border rounded shadow bg-white" onSubmit={onLoginSubmit}>
-                    <h2 className="text-center">Sign In</h2>
-                    <div className="form-floating mb-3">
-                        <input type="text" className="form-control" id="username" placeholder="Enter username" />
-                        <label htmlFor="username">Username</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input type="password" id="password" className="form-control" placeholder="Enter password" />
-                        <label htmlFor="password">Password</label>
-                    </div>
-                    <div>
-                        <button type="submit" className="btn btn-outline-primary w-100">Log In</button>
-                    </div>
-                </form>
-            </div>
+            <form className="p-4 border border-2 rounded" onSubmit={onLoginSubmit}>
+                <h2 className="text-center">Sign In</h2>
+                <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="username" placeholder="Enter username" />
+                    <label htmlFor="username">Username</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input type="password" id="password" className="form-control" placeholder="Enter password" />
+                    <label htmlFor="password">Password</label>
+                </div>
+                <div>
+                    <button type="submit" className="btn btn-outline-primary w-100">Log In</button>
+                </div>
+            </form>
         </>
     );
 }
