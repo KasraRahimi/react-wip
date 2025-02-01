25171ec22d74957934d46042bbf7c735040525d7
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-01-31 23:06:09 -0500

    feat: formatted and added more reactive text values

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,28 +1,58 @@
-
-const onLoginSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(e)
-}
+import { useState } from "react";
 
 function LoginForm() {
-    return (
-        <>
-            <form className="p-4 border border-2 rounded" onSubmit={onLoginSubmit}>
-                <h2 className="text-center">Sign In</h2>
-                <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="username" placeholder="Enter username" />
-                    <label htmlFor="username">Username</label>
-                </div>
-                <div className="form-floating mb-3">
-                    <input type="password" id="password" className="form-control" placeholder="Enter password" />
-                    <label htmlFor="password">Password</label>
-                </div>
-                <div>
-                    <button type="submit" className="btn btn-outline-primary w-100">Log In</button>
-                </div>
-            </form>
-        </>
-    );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    console.log(Array(data.entries()));
+    for (let [key, value] of data.entries()) console.info(`${key}: ${value}`);
+  };
+
+  const isDisabled = !username || !password;
+
+  return (
+    <>
+      <form className="p-4 border border-2 rounded" onSubmit={onLoginSubmit}>
+        <h2 className="text-center">Sign In</h2>
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            name="username"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className={`btn btn-outline-${isDisabled ? 'secondary' : 'primary'} w-100`}
+            disabled={isDisabled}
+          >
+            Log In
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default LoginForm;
