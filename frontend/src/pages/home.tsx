898f849779ca1861c54fc20333692dab72638d8d
KasraRahimi <kasra.rahimi@polymtl.ca>
2025-02-01 10:10:12 -0500

    refactor: changed let to const

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -4,7 +4,7 @@ import SignUpForm from "../components/SignUpForm";
 
 function Home() {
     const [isLogin, setIsLogin] = useState(true);
-    let btnMessage = isLogin ? "Don't have an account?" : "Have an account?";
+    const btnMessage = isLogin ? "Don't have an account?" : "Have an account?";
 
     const getForm = (isLoggingIn: boolean) => isLoggingIn ? <LoginForm /> : <SignUpForm />
 
@@ -13,7 +13,7 @@ function Home() {
             <div className="container d-flex flex-column align-items-center justify-content-center min-vh-100">
                 {getForm(isLogin)}
                 <button 
-                    onClick={(_) => setIsLogin(!isLogin)} 
+                    onClick={() => setIsLogin(!isLogin)} 
                     className="btn btn-primary my-3">
                     {btnMessage}
                 </button>
