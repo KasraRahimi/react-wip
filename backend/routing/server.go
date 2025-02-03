1f8a95d866e3d3d6eac5c9d7bd36cf8f49e20969
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-02-02 23:14:13 -0500

    feat: added cors middleware

--- backend/routing/server.go
+++ backend/routing/server.go
@@ -7,8 +7,16 @@ import (
 
 const API_URL = "/api"
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func GetServerMux() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.Handle(API_URL+"/auth/signup", &routes.SignUpHandler{})
+	mux.Handle(API_URL+"/auth/signup", corsMiddleware(&routes.SignUpHandler{}))
 	return mux
 }
