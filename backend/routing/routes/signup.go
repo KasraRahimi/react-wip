3a66ec46983b91b4519a670c807c17b84d88f2fa
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-02-02 23:13:51 -0500

    feat: made sign up handler handle post requests

--- backend/routing/routes/signup.go
+++ backend/routing/routes/signup.go
@@ -1,12 +1,36 @@
 package routes
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type signUpJson struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 type SignUpHandler struct{}
 
+func (h *SignUpHandler) handlePost(w http.ResponseWriter, r *http.Request) {
+	var data signUpJson
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Invalid json format")
+		return
+	}
+
+	fmt.Fprintf(w, "email: %s\nusername: %s\npassword: %s\n", data.Email, data.Username, data.Password)
+}
+
 func (h *SignUpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s", r.Method, r.URL)
+	switch r.Method {
+	case http.MethodPost:
+		h.handlePost(w, r)
+	default:
+		fmt.Fprintf(w, "ERROR! %s %s", r.Method, r.URL)
+	}
 }
