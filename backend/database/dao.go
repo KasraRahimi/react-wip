0d7eeac5add42ca3d626207ea8024438b8613b31
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2025-02-04 22:40:12 -0500

    feat: implemented create method for userDAO

--- backend/database/dao.go
+++ backend/database/dao.go
@@ -1,3 +1,18 @@
 package database
 
 type UserDAO struct{}
+
+func (u *UserDAO) Create(user User) error {
+	db, err := GetDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+	INSERT INTO users (email, username, passwordHash)
+	VALUES (?, ?, ?)
+	`, user.Email, user.Username, user.PasswordHash)
+
+	return err
+}
