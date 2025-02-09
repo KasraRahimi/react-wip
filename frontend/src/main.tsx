8ffaa5a2e0c377a19ae69d77f1dabdb91930315f
kasra012345@gmail.com <kasra012345@gmail.com>
2025-02-08 21:11:00 -0500

    feat: added store provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/home.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux'
+import store from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
